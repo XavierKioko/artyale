630845d34d82ea4935580d46da0f11d30788330b
munene-coder <john.ndwiga@strathmore.edu>
2021-06-17 19:25:44 +0300

    updated events modul

--- public/events.php
+++ public/events.php
@@ -1,26 +1,28 @@
+<?php
+session_start();
+?>
 <html>
 	<head>
 		<title>Public Events and Exhibitions</title>
 	
-	    <!-- Latest compiled and minified CSS -->
        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="style.css">
-		<link rel="stylesheet" type="text/css" href="main.css">
+	   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
+		<link rel="stylesheet" type="text/css" href="st.css">
+		<script src="script.js"></script>
 		<meta name="viewport" content="width=device-width,initial-scale=1.0">	
 	</head>
 	<body>
 		<nav>
-            <img href="index.html" src="images/yaleLogo.png" class="img" alt="logo"> 
+          <!--  <img href="index.html" src="images/yaleLogo.png" class="img" alt="logo"> -->
             <ul>
                 <li><a href="index.html">HOME</a></li>
                 <li><a href="about.html">ABOUT</a></li>
                 <li><a href="courses.html">COURSES</a></li>
-                <li><a href="events.html">EVENTS</a></li>              
+                <li><a href="events.php">EVENTS</a></li>              
                 <li><a href="news.html">NEWS</a></li>
                 <li><a href="contact.html">CONTACT</a></li>
             </ul>
         </nav>
-        
         
 		<!-- Slideshow container -->
 		<div class="Slideshow-container">
@@ -73,47 +75,84 @@
 	<span class="dot" onclick="currentSlide(4)"></span>
   </div>
   <div class="Secondary">
-	  <div class="moreevents" >
+	  
+	  <br>
+	  <div class="moreevents">
 
-		 <form action="eventsdetails.html">
-			<button type="submit" class="btn btn-outline-info">More Event Details</button>
+		 <form action="eventsdetails.php">
+			<button type="submit" class="btn btn-outline-primary">More Event Details</button>
 		  </form>
 		  
 	  </div> 
-	  <div class="exhibitions">
-		 <div class="head"style="background-color:#DCE1E3;height:100px;padding-top:50px;padding-bottom:90px;"><h1>Award Winning Exhibitions 2020</h1></div>  
-	    
-		 <div class="exhibitionsimages" style="background-color: #DCE1E3";>
-			 <div class="exhibitionsimg" style="background-color:white;">
-				<a href="img1.html"> <img src="images/img1.jpg" width="240px" height="200px"></a>
-				 <div id="image1des">Lorem ipsum dolor sit amet</div>
+	<hr>
+	  <div class="exhibitions" >
+		 <div class="evntspage_headers"  > 
+			  <h1>Top 5 Exhibitions 2020</h1>
+			</div>
+			<hr>
+	    <br>
+		 <div class="exhibitionsimages">
+			 <div class="exhibitionsimg">
+				<a href="exhibition1.html"> <img src="images/img1.jpg" ></a>
+				 <div id="image1des">Award winning</div>
 			 </div>
-			 <div class="exhibitionsimg" style="background-color:white">
-			<a href="img2.html"><img src="images/img2.jpg" width="240px" height="200px"></a>
-				<div id="image2des">Lorem ipsum dolor sit amet</div>
+			 <div class="exhibitionsimg"  >
+			<a href="img2.html"><img src="images/img2.jpg" ></a>
+				<div id="image2des">Exhibition 2</div>
 			</div>
-			<div class="exhibitionsimg" style="background-color:white">
-			<a href="img3.html"><img src="images/img3.jpg" width="240px" height="200px"></a>
-				<div id="image3des">Lorem ipsum dolor sit amet</div>
+			<div class="exhibitionsimg">
+			<a href="img3.html"><img src="images/img3.jpg" ></a>
+				<div id="image3des">Exhibition 3</div>
 			</div>
-			<div class="exhibitionsimg" style="background-color:white" >
-				<a href="img4.html"><img src="images/img4.jpg" width="240px" height="200px" ></a>
-				<div id="image4des">Lorem ipsum dolor sit amet</div>
+			<div class="exhibitionsimg">
+				<a href="img4.html"><img src="images/img4.jpg"  ></a>
+				<div id="image4des">Exhibition 4</div>
 			</div>
-			<div class="exhibitionsimg" style="background-color:white" >
-				<a href="img5.html"><img src="images/img5.jpg" width="240px" height="200px" ></a>
-				<div id="image4des">Lorem ipsum dolor sit amet</div>
+			<div class="exhibitionsimg">
+				<a href="img5.html"><img src="images/img5.jpg"  ></a>
+				<div id="image4des">Exhibition 5</div>
+			
 			</div>
 			
 		 </div>
+		 <hr>
 	  </div>
-	  <br><br>
+	  
+	  <br>
 	  <div class="moreexhibitions">
 		<button type="submit" class="btn btn-outline-primary">More Exhibitions</button>
 	</div>
-	<br><br>
+<hr>
+	<br>
 	
   </div>
+  
+  <div class="lowercontent">
+	  <div  class="lowcont" style="text-align:center">
+		 <div class="evntspage_headers" >
+			  <h1 >Sponsors of Our Events</h1></div>
+		<br><br>
+		 <div id="divToShowHide" class="BeforeScroll" style="display:flex;margin-left:300px">
+		 <div class="images" style="padding-left:70px">
+            <img src="images/IBM.png"alt="ibm" style="height:160px;width:200px;border-radius:100%">
+        </div>
+        <div class="images" style="padding-left:70px">
+            <img src="images/canon.png"alt="ibm" style="height:160px;width:200px;border-radius:100%">
+        </div>
+        <div class="images" style="padding-left:70px">
+            <img src="images/crown.PNG"alt="ibm" style="height:160px;width:200px;border-radius:100%">
+        </div>
+        <div class="images" style="padding-left:70px">
+            <img src="images/rockstar.jpg"alt="ibm" style="width:200px;border-radius:100%">
+        </div>
+        <div class="images" style="padding-left:70px">
+            <img src="images/bbc.jpg"alt="ibm" style="height:160px;width:200px;border-radius:100%">
+        </div>
+</div>
+</div>
+</div>
+
+<!-- javascript slideshow code-->
   <script>
 	var slideIndex = 0;
 	showSlides();
@@ -132,8 +171,9 @@
 	  }
 	  slides[slideIndex-1].style.display = "block";  
 	  dots[slideIndex-1].className += " active";
-	  setTimeout(showSlides, 1570); // Change image every 5 seconds
+	  setTimeout(showSlides, 1570); // Change image every 1.5 seconds
 	}
 	</script>
+
 	</body>
 </html>
---
+++ public/eventsdetails.php
@@ -0,0 +1,108 @@
+<?php
+session_start();
+$_SESSION['paintsession']="paintsession";
+?>
+<html>
+    <head>
+        <title>Events Details</title>
+        <link rel="stylesheet" type="text/css" href="st.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">	
+    </head>
+    <body >
+        <div class>
+            <div class="detailsheading">
+                <div class="evehdng" id="evheading"><h1>Events Details</h1></div>
+             <a  href="#yalemap" class="mapnav">Get Directions</a>
+             <a href="covid.html" class="mapnav">Covid 19 Protocols</a>
+              <div class="evebtn">
+                 
+                  <form action="events.php">
+                    <button type="submit" class="btn btn-outline-primary">Go Back</button></div> 
+                  </form> 
+            </div>
+            <hr>
+            </div>
+            <div id="events_details">
+                <div class="left_evnts"  >
+                    <h2>Paint Night</h3>
+                        <div class="pnt">
+                    <div><img src="images/paintnight.jpg" width="500px" height="400px"></div>
+                  
+                  <div style="width:600px;margin-left:250px">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque posuere odio in eros consectetur lacinia. Etiam eget dui sit amet ipsum tempus sagittis. Mauris elementum nisi nunc, quis sagittis risus pretium id. Sed enim ex, tristique ac tristique ac, facilisis at augue. Suspendisse viverra euismod dui et porta. Suspendisse potenti. In ut euismod velit. Ut a metus enim. Nam ut auctor lorem, nec auctor tortor. Quisque nisl tellus, lobortis at urna at, gravida bibendum turpis. Cras dignissim, tellus sed vehicula ornare, ipsum mauris iaculis tortor, a fringilla neque eros in turpis. Proin ut massa pellentesque, interdum tellus et, tincidunt ipsum. Curabitur id posuere lectus, id sodales velit. Integer vitae urna nis<br>
+                    <br><br>
+                    <form action="register.php">
+                        <input type="hidden" name="paintnight">
+                        <button type="submit" class="btn btn-outline-info">Attend Event</button>
+                    </form>
+                </div>
+                  
+                    </div>
+                </div>
+                
+                <div class="right_evnts">
+                       
+                         <div class="pnt">
+                            <div style="width:500px;margin-left:300px;margin-top:70px">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque posuere odio in eros consectetur lacinia. Etiam eget dui sit amet ipsum tempus sagittis. Mauris elementum nisi nunc, quis sagittis risus pretium id. Sed enim ex, tristique ac tristique ac, facilisis at augue. Suspendisse viverra euismod dui et porta. Suspendisse potenti. In ut euismod velit. Ut a metus enim. Nam ut auctor lorem, nec auctor tortor. Quisque nisl tellus, lobortis at urna at, gravida bibendum turpis. Cras dignissim, tellus sed vehicula ornare, ipsum mauris iaculis tortor, a fringilla neque eros in turpis. Proin ut massa pellentesque, interdum tellus et, tincidunt ipsum. Curabitur id posuere lectus, id sodales velit. Integer vitae urna nis
+                            <br><br>
+                            <form action="register.php">
+                                <button type="submit" class="btn btn-outline-info">Attend Event</button>
+                            </form>
+                            </div>
+                            
+                    <div style="margin-left:250px;margin-top:30px" >
+                        <h2 >Color Festival</h2>   
+                        <img src="images/colorfest.jpg" width="500px" height="400px"></div>
+                       
+                    </div>
+                </div>
+                
+                <div class="left_evnts">
+                    <h2>Craft Day</h3>
+                        <div class="pnt">
+                    <div><img src="images/craft.jpg" width="500px" height="400px"></div>
+                  
+                  <div style="width:600px;margin-left:250px">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque posuere odio in eros consectetur lacinia. Etiam eget dui sit amet ipsum tempus sagittis. Mauris elementum nisi nunc, quis sagittis risus pretium id. Sed enim ex, tristique ac tristique ac, facilisis at augue. Suspendisse viverra euismod dui et porta. Suspendisse potenti. In ut euismod velit. Ut a metus enim. Nam ut auctor lorem, nec auctor tortor. Quisque nisl tellus, lobortis at urna at, gravida bibendum turpis. Cras dignissim, tellus sed vehicula ornare, ipsum mauris iaculis tortor, a fringilla neque eros in turpis. Proin ut massa pellentesque, interdum tellus et, tincidunt ipsum. Curabitur id posuere lectus, id sodales velit. Integer vitae urna nis<br>
+                    <br><br>
+                    <form action="register.php">
+                        <button type="submit" class="btn btn-outline-info">Attend Event</button>
+                    </form>
+                </div>
+                  
+                    </div>
+                </div>
+                
+                <div class="right_evnts">
+                       
+                    <div class="pnt">
+                       <div style="width:500px;margin-left:300px;margin-top:70px">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque posuere odio in eros consectetur lacinia. Etiam eget dui sit amet ipsum tempus sagittis. Mauris elementum nisi nunc, quis sagittis risus pretium id. Sed enim ex, tristique ac tristique ac, facilisis at augue. Suspendisse viverra euismod dui et porta. Suspendisse potenti. In ut euismod velit. Ut a metus enim. Nam ut auctor lorem, nec auctor tortor. Quisque nisl tellus, lobortis at urna at, gravida bibendum turpis. Cras dignissim, tellus sed vehicula ornare, ipsum mauris iaculis tortor, a fringilla neque eros in turpis. Proin ut massa pellentesque, interdum tellus et, tincidunt ipsum. Curabitur id posuere lectus, id sodales velit. Integer vitae urna nis
+                       <br><br>
+                       <form action="register.php">
+                           <button type="submit" class="btn btn-outline-info">Attend Event</button>
+                       </form>
+                       </div>
+                       
+               <div style="margin-left:250px;margin-top:30px">
+                   <h2 >Kite Flying Festival</h2>   
+                   <img src="images/kitefly.jpg" width="500px" height="400px"></div>
+               </div>
+           </div>
+           <br>
+       
+           <div class="directions" id="yalemap">
+            
+            <br><br>
+            <div class="map">
+            <h1 >Get Directions to the Events Location</h1>
+            <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2997.0217716864386!2d-72.93315204891874!3d41.30838997916973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e7d9b31fff4103%3A0xf5dffb2781e299ac!2sYale%20University%20Art%20Gallery!5e0!3m2!1sen!2sus!4v1623574350408!5m2!1sen!2sus" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+          </div>
+          <div class="toparrow">
+          <a  href="#evheading"><img style="width:50px;height:50px;margin-left:40px"  src="images/top.png"></a>
+          <p ><a href="#evheading">Go back to top</a></p>
+          </div>
+        </div>
+            </div>
+        </div>
+        <hr>
+    </body>
+</html>
